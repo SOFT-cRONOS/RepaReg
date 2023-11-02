b50c7152023d4b0535a1b87c767e140aa3a0693b
SOFT-cRONOS <103128033+SOFT-cRONOS@users.noreply.github.com>
2023-11-02 11:07:07 -0300

    panel y gestion usuario

--- Front/assets/js/script.js
+++ Front/assets/js/script.js
@@ -25,6 +25,10 @@ document.addEventListener('DOMContentLoaded', () => {
                   <td><img src=${cliente.avatar} width="50px"></td>
                   <td>${cliente.name}</td>
                   <td>${cliente.email}</td>
+                  <td>
+                  <a href="https://api.escuelajs.co/api/v1/users?id=${cliente.id}" class="btn-edit"> <i class="fas fa-edit"></i>Editar</a>
+                  <a href="https://api.escuelajs.co/api/v1/users?id=${cliente.id}" class="btn btn-baja">Baja</a>
+                  </td>
               `;
               tbody.appendChild(fila);
           });
@@ -42,3 +46,77 @@ document.addEventListener('DOMContentLoaded', () => {
 //       Fin Api
 // #############
 
+// ##################################################################################
+//                                 GRAFICOS DASHBOARD
+// ##################################################################################
+
+    // Paleta de colores
+var colorPrimario = "#3c3a78";// violeta obscuro
+var colorSecundario = "#948fc4"; // claro
+var colorTerciario = "#77669d"; // medio
+
+// Datos de ejemplo para los gráficos
+  var dataLine = {
+    labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6', 'Día 7'],
+    datasets: [{
+      label: 'Ingresos en los últimos 7 días',
+    //   array de datos
+      data: [100, 150, 120, 200, 180, 250, 210],
+      fill: false,
+      borderColor: colorPrimario,
+      pointRadius: 5,
+      backgroundColor: 'transparent',
+      borderWidth: 4,
+      pointBackgroundColor: colorPrimario
+    }]
+  };
+
+  var dataBar = {
+    labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6', 'Día 7'],
+    datasets: [{
+      label: 'Ingresos de equipos al taller en los últimos 7 días',
+      //   array de datos
+      data: [10, 15, 12, 20, 18, 25, 21],
+      backgroundColor: colorTerciario
+    }]
+  };
+
+  var dataPie = {
+    labels: ['Ventas', 'Reparaciones'],
+    datasets: [{
+        //   array de datos
+      data: [70, 30],
+      backgroundColor: [colorPrimario, colorSecundario]
+    }]
+  };
+
+  // Configuración de los gráficos
+  var chartOptions = {
+    responsive: true,
+    maintainAspectRatio: true,
+  };
+
+  var ctxLine = document.getElementById('lineChart').getContext('2d');
+  var lineChart = new Chart(ctxLine, {
+    type: 'line',
+    data: dataLine,
+    
+  });
+
+  var ctxBar = document.getElementById('barChart').getContext('2d');
+  var barChart = new Chart(ctxBar, {
+    type: 'bar',
+    data: dataBar,
+    options: chartOptions
+  });
+
+  var ctxPie = document.getElementById('pieChart').getContext('2d');
+  var pieChart = new Chart(ctxPie, {
+    type: 'pie',
+    data: dataPie,
+    options: chartOptions
+  });
+
+// ##################################################################################
+//                                FIN GRAFICOS DASHBOARD
+// ##################################################################################
