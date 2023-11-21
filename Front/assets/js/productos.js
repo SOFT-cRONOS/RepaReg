6f4e270c781f47ce5e188525141d15c6ab57903d
Patricia Roxana <82889906+PatriciaRoxana@users.noreply.github.com>
2023-11-20 22:29:16 -0300

    Arregla iconos en la sidebar. Comienza implementacion modal de productos en modo nuevo, ver y edicion.

--- Front/assets/js/productos.js
+++ Front/assets/js/productos.js
@@ -1,13 +1,137 @@
+const MODOS_MODAL = {
+  nuevo: "nuevo",
+  ver: "ver",
+  edicion: "edicion",
+};
+
+const modoModal = MODOS_MODAL.nuevo;
+
 const modalProductos = new bootstrap.Modal("#modal-productos");
+
+const btnEditarProducto = document.getElementById("btn-editar-producto");
 
 const btnNuevoProducto = document.getElementById("btn-nuevo-producto");
 const btnGuardarProducto = document.getElementById("btn-guardar-producto");
+const btnBuscarProducto = document.getElementById("btn-buscar-producto");
+
+const inputNombre = document.getElementById("nombre");
+const inputPrecio = document.getElementById("precio");
+const inputStock = document.getElementById("stock");
+
+const tituloModalProductos = document.getElementById("titulo-modal-productos");
+
+const obtenerProductos = () => {
+  const productos = [
+    {
+      id: 15,
+      nombre: "Monitor Samsung",
+      precio: 2530000,
+      stock: 5,
+      fechaIngreso: "20/11/2023",
+    },
+    {
+      id: 235,
+      nombre: "Notebook Lenovo",
+      precio: 8565000,
+      stock: 2,
+      fechaIngreso: "15/10/2023",
+    },
+    {
+      id: 38,
+      nombre: "Webcam LG",
+      precio: 57300,
+      stock: 1,
+      fechaIngreso: "10/11/2023",
+    },
+    {
+      id: 49,
+      nombre: "Mouse Logitech",
+      precio: 59630,
+      stock: 3,
+      fechaIngreso: "13/11/2023",
+    },
+    {
+      id: 545,
+      nombre: "Teclado Microsoft",
+      precio: 123124,
+      stock: 20,
+      fechaIngreso: "08/11/2023",
+    },
+    {
+      id: 621,
+      nombre: "Mouse Genius",
+      precio: 15600,
+      stock: 15,
+      fechaIngreso: "12/08/2023",
+    },
+    {
+      id: 70,
+      nombre: "Notebook HP",
+      precio: 1560000,
+      stock: 19,
+      fechaIngreso: "05/08/2023",
+    },
+  ];
+
+  mostrarProductosEnTabla(productos);
+};
+
+const mostrarProductosEnTabla = (productos) => {
+  const datosTabla = document.getElementById("datos-tabla");
+
+  let html = "";
+
+  //Dibujar la tabla de productos
+  productos.forEach((producto) => {
+    html += `<tr>
+              <td>${producto.nombre}</td>
+              <td>$ ${producto.precio}</td>
+              <td>${producto.stock}</td>
+              <td>${producto.fechaIngreso}</td>
+
+              <td>
+                <button data-id-producto="${producto.id}" class="btn btn-ver" >
+                  <span data-feather="eye"></span>Ver
+                </button>
+
+                <button data-id-producto="${producto.id}" class="btn btn-eliminar">
+                  <span data-feather="trash"></span>Eliminar
+                </button>
+              </td>
+            </tr>`;
+  });
+
+  datosTabla.innerHTML = html;
+
+  const botonesVer = document.getElementsByClassName("btn-ver");
+  const botonesEliminar = document.getElementsByClassName("btn-eliminar");
+
+  for (const botonVer of botonesVer) {
+    botonVer.addEventListener("click", mostrarModalDetalleProducto);
+  }
+
+  for (const botonEliminar of botonesEliminar) {
+    botonEliminar.addEventListener("click", eliminarProducto);
+  }
+};
+
+const mostrarModalDetalleProducto = (event) => {
+  const idProducto = event.target.getAttribute("data-id-producto");
+
+  console.log("Mostrar el detalle del producto", idProducto);
+
+  cambiarEstadoModal(MODOS_MODAL.ver);
+  modalProductos.show();
+};
+
+const eliminarProducto = (event) => {
+  const idProducto = event.target.getAttribute("data-id-producto");
+
+  alert("Eliminar producto " + idProducto);
+};
 
 const mostrarModalProductos = () => {
-  document.getElementById("nombre").value = "";
-  document.getElementById("precio").value = "";
-  document.getElementById("stock").value = "";
-
+  cambiarEstadoModal(MODOS_MODAL.nuevo);
   modalProductos.show();
 };
 
@@ -21,5 +145,60 @@ const guardarProducto = () => {
   modalProductos.hide();
 };
 
+const buscarProducto = () => {
+  const cajaBusqueda = document.getElementById("caja-busqueda");
+
+  const textoBuscado = cajaBusqueda.value;
+
+  console.log("Buscar el producto...", textoBuscado);
+};
+
+const editarProducto = () => {
+  cambiarEstadoModal(MODOS_MODAL.edicion);
+};
+
+const cambiarEstadoModal = (nuevoModoModal) => {
+  if (nuevoModoModal === MODOS_MODAL.nuevo) {
+    tituloModalProductos.innerHTML = "Nuevo Producto";
+
+    inputNombre.value = "";
+    inputPrecio.value = "";
+    inputStock.value = "";
+
+    inputNombre.disabled = false;
+    inputPrecio.disabled = false;
+    inputStock.disabled = false;
+
+    btnEditarProducto.style.display = "none";
+  } else if (nuevoModoModal === MODOS_MODAL.ver) {
+    tituloModalProductos.innerHTML = "Ver producto";
+
+    inputNombre.value = "XXXX x x x ";
+    inputPrecio.value = "55555";
+    inputStock.value = "99999";
+
+    inputNombre.disabled = true;
+    inputPrecio.disabled = true;
+    inputStock.disabled = true;
+
+    btnEditarProducto.style.display = "block";
+  } else if (nuevoModoModal === MODOS_MODAL.edicion) {
+    tituloModalProductos.innerHTML = "Editar producto";
+
+    inputNombre.disabled = false;
+    inputPrecio.disabled = false;
+    inputStock.disabled = false;
+
+    btnEditarProducto.style.display = "none";
+  }
+};
+
 btnNuevoProducto.addEventListener("click", mostrarModalProductos);
 btnGuardarProducto.addEventListener("click", guardarProducto);
+btnBuscarProducto.addEventListener("click", buscarProducto);
+
+console.log({ btnEditarProducto });
+
+btnEditarProducto.addEventListener("click", editarProducto);
+
+obtenerProductos();
--- Front/assets/js/script.js
+++ Front/assets/js/script.js
@@ -1,14 +1,9 @@
-
-
-
-
-
-// #############  
+// #############
 //       Api
 // #############
 // Función para cargar la lista de clientes desde la API
 const cargarClientes = () => {
-  const nuevoClienteForm = document.getElementById('nuevo-cliente-form');
+  /* const nuevoClienteForm = document.getElementById('nuevo-cliente-form');
   const tabla = document.getElementById('tabla-clientes');
   const tbody = document.getElementById('datos-tabla-clientes');
 
@@ -37,191 +32,199 @@ const cargarClientes = () => {
             feather.replace();
         });
     })
-    .catch(error => console.error('Error al obtener la lista de clientes:', error));
-
+    .catch(error => console.error('Error al obtener la lista de clientes:', error)); */
 };
 
-// #############  
+// #############
 //       Fin Api
 // #############
-
-
-
 
 // ##################################################################################
 //                                 GRAFICOS DASHBOARD
 // ##################################################################################
-    // graficacion
-      // Paleta de colores
-      var colorPrimario = "#3c3a78";// violeta obscuro
-      var colorSecundario = "#948fc4"; // claro
-      var colorTerciario = "#77669d"; // medio
+// graficacion
+// Paleta de colores
+var colorPrimario = "#3c3a78"; // violeta obscuro
+var colorSecundario = "#948fc4"; // claro
+var colorTerciario = "#77669d"; // medio
 
-      // Datos de ejemplo para los gráficos
-        var dataLine = {
-          labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6', 'Día 7'],
-          datasets: [{
-            label: 'Ingresos en los últimos 7 días',
-          //   array de datos
-            data: [100, 150, 120, 200, 180, 250, 210],
-            fill: false,
-            borderColor: colorPrimario,
-            pointRadius: 5,
-            backgroundColor: 'transparent',
-            borderWidth: 4,
-            pointBackgroundColor: colorPrimario
-          }]
-        };
+// Datos de ejemplo para los gráficos
+var dataLine = {
+  labels: ["Día 1", "Día 2", "Día 3", "Día 4", "Día 5", "Día 6", "Día 7"],
+  datasets: [
+    {
+      label: "Ingresos en los últimos 7 días",
+      //   array de datos
+      data: [100, 150, 120, 200, 180, 250, 210],
+      fill: false,
+      borderColor: colorPrimario,
+      pointRadius: 5,
+      backgroundColor: "transparent",
+      borderWidth: 4,
+      pointBackgroundColor: colorPrimario,
+    },
+  ],
+};
 
-        var dataBar = {
-          labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6', 'Día 7'],
-          datasets: [{
-            label: 'Ingresos de equipos al taller en los últimos 7 días',
-            //   array de datos
-            data: [10, 15, 12, 20, 18, 25, 21],
-            backgroundColor: colorTerciario
-          }]
-        };
+var dataBar = {
+  labels: ["Día 1", "Día 2", "Día 3", "Día 4", "Día 5", "Día 6", "Día 7"],
+  datasets: [
+    {
+      label: "Ingresos de equipos al taller en los últimos 7 días",
+      //   array de datos
+      data: [10, 15, 12, 20, 18, 25, 21],
+      backgroundColor: colorTerciario,
+    },
+  ],
+};
 
-        var dataPie = {
-          labels: ['Ventas', 'Reparaciones'],
-          datasets: [{
-              //   array de datos
-            data: [70, 30],
-            backgroundColor: [colorPrimario, colorSecundario]
-          }]
-        };
+var dataPie = {
+  labels: ["Ventas", "Reparaciones"],
+  datasets: [
+    {
+      //   array de datos
+      data: [70, 30],
+      backgroundColor: [colorPrimario, colorSecundario],
+    },
+  ],
+};
 
-        // Configuración de los gráficos
-        var chartOptions = {
-          responsive: true,
-          maintainAspectRatio: true,
-        };
+// Configuración de los gráficos
+var chartOptions = {
+  responsive: true,
+  maintainAspectRatio: true,
+};
 
-        var ctxLine = document.getElementById('lineChart').getContext('2d');
-        var lineChart = new Chart(ctxLine, {
-          type: 'line',
-          data: dataLine,
-          
-        });
+var ctxLine = document.getElementById("lineChart").getContext("2d");
+var lineChart = new Chart(ctxLine, {
+  type: "line",
+  data: dataLine,
+});
 
-        var ctxBar = document.getElementById('barChart').getContext('2d');
-        var barChart = new Chart(ctxBar, {
-          type: 'bar',
-          data: dataBar,
-          options: chartOptions
-        });
+var ctxBar = document.getElementById("barChart").getContext("2d");
+var barChart = new Chart(ctxBar, {
+  type: "bar",
+  data: dataBar,
+  options: chartOptions,
+});
 
-        var ctxPie = document.getElementById('pieChart').getContext('2d');
-        var pieChart = new Chart(ctxPie, {
-          type: 'pie',
-          data: dataPie,
-          options: chartOptions
-        });
-    // Fin graficacion
+var ctxPie = document.getElementById("pieChart").getContext("2d");
+var pieChart = new Chart(ctxPie, {
+  type: "pie",
+  data: dataPie,
+  options: chartOptions,
+});
+// Fin graficacion
 
-    //  funcion animacion de numeros 
-    function animar(){
-      (function ($) {
-        $.fn.countTo = function (options) {
-          options = options || {};
-          
-          return $(this).each(function () {
-            // set options for current element
-            var settings = $.extend({}, $.fn.countTo.defaults, {
-              from:            $(this).data('from'),
-              to:              $(this).data('to'),
-              speed:           $(this).data('speed'),
-              refreshInterval: $(this).data('refresh-interval'),
-              decimals:        $(this).data('decimals')
-            }, options);
-            
-            // how many times to update the value, and how much to increment the value on each update
-            var loops = Math.ceil(settings.speed / settings.refreshInterval),
-              increment = (settings.to - settings.from) / loops;
-            
-            // references & variables that will change with each update
-            var self = this,
-              $self = $(this),
-              loopCount = 0,
-              value = settings.from,
-              data = $self.data('countTo') || {};
-            
-            $self.data('countTo', data);
-            
-            // if an existing interval can be found, clear it first
-            if (data.interval) {
-              clearInterval(data.interval);
-            }
-            data.interval = setInterval(updateTimer, settings.refreshInterval);
-            
-            // initialize the element with the starting value
-            render(value);
-            
-            function updateTimer() {
-              value += increment;
-              loopCount++;
-              
-              render(value);
-              
-              if (typeof(settings.onUpdate) == 'function') {
-                settings.onUpdate.call(self, value);
-              }
-              
-              if (loopCount >= loops) {
-                // remove the interval
-                $self.removeData('countTo');
-                clearInterval(data.interval);
-                value = settings.to;
-                
-                if (typeof(settings.onComplete) == 'function') {
-                  settings.onComplete.call(self, value);
-                }
-              }
-            }
-            
-            function render(value) {
-              var formattedValue = settings.formatter.call(self, value, settings);
-              $self.html(formattedValue);
-            }
-          });
-        };
-        
-        $.fn.countTo.defaults = {
-          from: 0,               // the number the element should start at
-          to: 0,                 // the number the element should end at
-          speed: 1000,           // how long it should take to count between the target numbers
-          refreshInterval: 100,  // how often the element should be updated
-          decimals: 0,           // the number of decimal places to show
-          formatter: formatter,  // handler for formatting the value before rendering
-          onUpdate: null,        // callback method for every time the element is updated
-          onComplete: null       // callback method for when the element finishes updating
-        };
-        
-        function formatter(value, settings) {
-          return value.toFixed(settings.decimals);
+//  funcion animacion de numeros
+function animar() {
+  (function ($) {
+    $.fn.countTo = function (options) {
+      options = options || {};
+
+      return $(this).each(function () {
+        // set options for current element
+        var settings = $.extend(
+          {},
+          $.fn.countTo.defaults,
+          {
+            from: $(this).data("from"),
+            to: $(this).data("to"),
+            speed: $(this).data("speed"),
+            refreshInterval: $(this).data("refresh-interval"),
+            decimals: $(this).data("decimals"),
+          },
+          options
+        );
+
+        // how many times to update the value, and how much to increment the value on each update
+        var loops = Math.ceil(settings.speed / settings.refreshInterval),
+          increment = (settings.to - settings.from) / loops;
+
+        // references & variables that will change with each update
+        var self = this,
+          $self = $(this),
+          loopCount = 0,
+          value = settings.from,
+          data = $self.data("countTo") || {};
+
+        $self.data("countTo", data);
+
+        // if an existing interval can be found, clear it first
+        if (data.interval) {
+          clearInterval(data.interval);
         }
-      }(jQuery));
-      
-      jQuery(function ($) {
-        // custom formatting example
-        $('.count-number').data('countToOptions', {
-        formatter: function (value, options) {
-          return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+        data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+        // initialize the element with the starting value
+        render(value);
+
+        function updateTimer() {
+          value += increment;
+          loopCount++;
+
+          render(value);
+
+          if (typeof settings.onUpdate == "function") {
+            settings.onUpdate.call(self, value);
+          }
+
+          if (loopCount >= loops) {
+            // remove the interval
+            $self.removeData("countTo");
+            clearInterval(data.interval);
+            value = settings.to;
+
+            if (typeof settings.onComplete == "function") {
+              settings.onComplete.call(self, value);
+            }
+          }
         }
-        });
-        
-        // start all the timers
-        $('.timer').each(count);  
-        
-        function count(options) {
-        var $this = $(this);
-        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
-        $this.countTo(options);
+
+        function render(value) {
+          var formattedValue = settings.formatter.call(self, value, settings);
+          $self.html(formattedValue);
         }
       });
+    };
+
+    $.fn.countTo.defaults = {
+      from: 0, // the number the element should start at
+      to: 0, // the number the element should end at
+      speed: 1000, // how long it should take to count between the target numbers
+      refreshInterval: 100, // how often the element should be updated
+      decimals: 0, // the number of decimal places to show
+      formatter: formatter, // handler for formatting the value before rendering
+      onUpdate: null, // callback method for every time the element is updated
+      onComplete: null, // callback method for when the element finishes updating
+    };
+
+    function formatter(value, settings) {
+      return value.toFixed(settings.decimals);
     }
-    //  fin funcion animacion de numeros 
+  })(jQuery);
+
+  jQuery(function ($) {
+    // custom formatting example
+    $(".count-number").data("countToOptions", {
+      formatter: function (value, options) {
+        return value
+          .toFixed(options.decimals)
+          .replace(/\B(?=(?:\d{3})+(?!\d))/g, ",");
+      },
+    });
+
+    // start all the timers
+    $(".timer").each(count);
+
+    function count(options) {
+      var $this = $(this);
+      options = $.extend({}, options || {}, $this.data("countToOptions") || {});
+      $this.countTo(options);
+    }
+  });
+}
+//  fin funcion animacion de numeros
 
 // ##################################################################################
 //                                FIN GRAFICOS DASHBOARD
