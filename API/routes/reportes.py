3ec8fdae1591c32cb7c24d539e1c66e6d0a59475
Matias Rivadeneira <matiasriv66@hotmail.com>
2023-11-30 17:21:58 -0300

    endpoint reportes

--- API/routes/reportes.py
+++ API/routes/reportes.py
@@ -32,78 +32,49 @@ def obtener_reporte_total_compras_por_cliente():
     return jsonify(reporteList)
 
 
-@app.route('/reportes/<int:idreport>', methods=['GET'])
+app.route('/reportes/<int:idreport>', methods=['GET'])
 def get_Report(idreport):
-    cur = mysql.connection.cursor()
-    if idreport == 1:
-        cur.execute('''SELECT
-                        DATE_FORMAT(fecha, '%d/%m') AS fecha,
-                        SUM(total) AS monto_total
-                        FROM venta
-                        GROUP BY DATE_FORMAT(fecha, '%d/%m')
-                        ORDER BY fecha
-                        LIMIT 7;
-                        ''')
-        data = cur.fetchall()
+    try:
+        params = {'id_usuario': session['id']}
+        cur = mysql.connection.cursor()
+        if idreport == 1:
+            cur.execute('''CALL lastWeekSell(%s);''', (session['id'],))
+            data = cur.fetchall()
 
-        reporteList = []
-        for row in data:
-            #responde sin modelo de clase
-            reporteList.append( {"dia": row[0], "total": row[1] } ) 
-        
-        
-        cur.close()
+            reporteList = []
+            for row in data:
+                #responde sin modelo de clase
+                reporteList.append( {"dia": row[0], "total": row[1] } ) 
+            
+            
+            cur.close()
 
+            return jsonify(reporteList)
+        elif idreport == 2:
+            cur.execute('''CALL lastSellsCategory(%s);''', (session['id'],))
+            data = cur.fetchall()
+
+            reporteList = []
+            for row in data:
+                #responde sin modelo de clase
+                reporteList.append( {"categoria": row[0], "cantidad": row[1]} ) 
+            
+            
+            cur.close()
+
+            return jsonify(reporteList)
+        elif idreport == 3:
+            #* comodin en mysql
+            cur.execute('''CALL lastSellServices(%s);''', (session['id'],))
+            data = cur.fetchall()
+
+            reporteList = []
+            for row in data:
+                #responde sin modelo de clase
+                reporteList.append( {"fecha": row[0], "cantidad": row[1]} ) 
+            
+            cur.close()
         return jsonify(reporteList)
-    elif idreport == 2:
-        cur.execute('''SELECT
-                        categoria.nombre AS categoria,
-                        COUNT(*) AS cantidad_ventas
-                        FROM
-                        detalle_venta
-                        INNER JOIN
-                        producto
-                        ON
-                        detalle_venta.id_producto = producto.id
-                        INNER JOIN
-                        categoria
-                        ON
-                        producto.id_categoria = categoria.id
-                        GROUP BY
-                        categoria.nombre''')
-        data = cur.fetchall()
-
-        reporteList = []
-        for row in data:
-             #responde sin modelo de clase
-            reporteList.append( {"categoria": row[0], "cantidad": row[1]} ) 
-        
-        
-        cur.close()
-
-        return jsonify(reporteList)
-    elif idreport == 3:
-        #* comodin en mysql
-        cur.execute('''SELECT
-                            DATE_FORMAT(v.fecha, '%d/%m') AS fecha,
-                            COUNT(*) AS cantventas
-                        FROM
-                            venta v
-                            INNER JOIN detalle_venta dv ON v.id = dv.id_venta
-                            INNER JOIN servicio s ON dv.id_servicio = s.id
-                        WHERE
-                            v.fecha >= CURDATE() - INTERVAL 7 DAY
-                        GROUP BY
-                            DATE_FORMAT(fecha, '%d/%m')
-                        ORDER BY
-                            fecha;''')
-        data = cur.fetchall()
-
-        reporteList = []
-        for row in data:
-             #responde sin modelo de clase
-            reporteList.append( {"fecha": row[0], "cantidad": row[1]} ) 
-        
-        cur.close()
-
-        return jsonify(reporteList)
+    except Exception as e:
+        print(e)
+        return {"error en graficacion"}
